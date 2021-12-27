804ee3fb8ae4cec6d2e1cbe1dd5fafa98c4568b1
lev-nabiulin <levnabiulin@yandex.ru>
2021-12-27 08:53:37 +0300

    s

--- yatube_api/api/mixins.py
+++ yatube_api/api/mixins.py
@@ -1,5 +1,6 @@
 from rest_framework import mixins, viewsets
 
+
 class CustomGetOrPostViewSet(mixins.CreateModelMixin,
                              mixins.ListModelMixin,
                              mixins.RetrieveModelMixin,
